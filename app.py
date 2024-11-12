2c4406f868fd2b8e6bb161bff0bc64d3d355235b
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-12 08:28:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,17 @@ def home():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+handler = RotatingFileHandler('/app/logs/flask_app.log', maxBytes=10000, backupCount=3)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+
+@app.route('/')
+def home():
+    app.logger.info("Homepage accessed")
+    return "Welcome to T2S!"
